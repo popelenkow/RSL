d6f962fa8e0c6ce947f51383fbe619743d82df50
popelenkow <popelenkow@gmail.com>
2017-09-14 06:54:41 +0700

    Learn AvaloniaMVVM 0.6 (Hierarchies & Navigation)

---
+++ AvaloniaCoreMVVM2/Views/MainView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaCoreMVVM2.Views
+{
+    public class MainView : Window
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+            this.AttachDevTools();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
